de7d5404fd2546a2ca94113960f4ad04df61b849
bilalEBR <bilal999ebrahim@gmail.com>
2024-06-07 23:28:20 +0300

    Prime factorization_java version two

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -1,15 +1,21 @@
-
 package tools;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PrimeFactorization {
+	
     public static List<Integer> primeFactors(int number) {
+    	
         int n = number;
+        
         List<Integer> factors = new ArrayList<>();
+        
         for (int i = 2; i <= n / i; i++) {
+        	
             while (n % i == 0) {
+            	
                 factors.add(i);
                 n /= i;
             }
@@ -21,10 +27,31 @@ public class PrimeFactorization {
     }
 
     public static void main(String[] args) {
-        System.out.println("Prime factors of 100 are :");
-        for (Integer factor : primeFactors(100)) {
+    	
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        
+        int number = scanner.nextInt();
+        
+        scanner.close();
+
+        System.out.println("Prime factors of " + number + ":");
+        
+        for (Integer factor : primeFactors(number)) {
+        	
             System.out.println(factor);
         }
     }
-}
+    }
+
+
+
+
+
+
+
+
+
+
 
